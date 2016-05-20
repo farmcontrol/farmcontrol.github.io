1e1f25ed2e22eaa37ef9e9f7e0be61ffe26e6a22
rafagalani <galani.rafael@gmail.com>
2016-05-20 15:50:19 -0300

    Configuração de temperatura e umidade
    Log:
    
    - Botões e componentes da modal box no HTML
    - Ícone de config
    - CSS dos elementos HTML
    - JS com validações

--- js/app.js
+++ js/app.js
@@ -3,10 +3,20 @@ var Channel = {};
 var Feeds = [];
 var internal = $('.internal');
 var external = $('.external');
-var minTemperature = 10;
-var maxTemperature = 25;
-var minHumidity = 70;
-var maxHumidity = 90;
+
+var externalRange = {
+	minTemperature: 10,
+	maxTemperature: 25,
+	minHumidity: 	70,
+	maxHumidity: 	90
+}
+var internalRange = {
+	minTemperature: 10,
+	maxTemperature: 25,
+	minHumidity: 	70,
+	maxHumidity: 	90
+}
+
 var $loader = $('.loader');
 
 function getData(callbackFunction){
@@ -49,29 +59,29 @@ function refreshView(){
 		if (i == Feeds.length - 1) {
 			// INTERNAL TEMPERATURE INFOS
 			$htmlObject = $('.internal').find('.temperature');
-			$htmlObject.attr('class', 'temperature ' + getTemperatureColorClass(inTemperature));
+			$htmlObject.attr('class', 'temperature ' + getTemperatureColorClass(inTemperature, internalRange));
 			$htmlObject.html(inTemperature + 'C');
 			$htmlObject = $('.internal').find('.humidity');
-			$htmlObject.attr('class', 'humidity ' + getHumidityColorClass(inHumidity));
+			$htmlObject.attr('class', 'humidity ' + getHumidityColorClass(inHumidity, internalRange));
 			$htmlObject.html( (!!inHumidity) ? 'umidade: ' + inHumidity + '%' : '---');
 
 			// EXTERNAL TEMPERATURE INFOS
 			$htmlObject = $('.external').find('.temperature');
-			$htmlObject.attr('class', 'temperature ' + getTemperatureColorClass(outTemperature));
+			$htmlObject.attr('class', 'temperature ' + getTemperatureColorClass(outTemperature, externalRange));
 			$htmlObject.html(outTemperature + 'C');
 			$htmlObject = $('.external').find('.humidity');
-			$htmlObject.attr('class', 'humidity ' + getHumidityColorClass(outHumidity));
+			$htmlObject.attr('class', 'humidity ' + getHumidityColorClass(outHumidity, externalRange));
 			$htmlObject.html( (!!outHumidity) ? 'umidade: ' + outHumidity + '%' : '---');
 
 			setLastUpdate(new Date(Feeds[i].created_at));
 		} else{
 			// prrenche table de informações internas
 			$htmlObject = $('.internal .infosTable');
-			addLine($htmlObject, inTemperature, inHumidity);
+			addLine($htmlObject, inTemperature, inHumidity, internalRange);
 
 			// prrenche table de informações externas
 			$htmlObject = $('.external .infosTable');
-			addLine($htmlObject, outTemperature, outHumidity);
+			addLine($htmlObject, outTemperature, outHumidity, externalRange);
 		};
 	};
 }
@@ -82,8 +92,8 @@ function createInfoTable($htmlElement){
 	$htmlElement.find('.table').append('<tr><th>Temperatura</th><th>Umidade</th></tr>')
 }
 
-function addLine($htmlElement, temperature, humidity){
-	$htmlElement.find('.table').append('<tr><td class="' + getTemperatureColorClass(temperature) + '">' + temperature + 'C</td><td class="' + getHumidityColorClass(humidity) + '">' + humidity + '%</td></tr>');
+function addLine($htmlElement, temperature, humidity, location){
+	$htmlElement.find('.table').append('<tr><td class="' + getTemperatureColorClass(temperature, location) + '">' + temperature + 'C</td><td class="' + getHumidityColorClass(humidity, location) + '">' + humidity + '%</td></tr>');
 }
 
 function setLastUpdate(date){
@@ -92,17 +102,39 @@ function setLastUpdate(date){
 	$('.dateTime').html('Atualizado em ' + str_data + ' ' + str_hora);
 }
 
-function getTemperatureColorClass(temperature){
-	if (temperature < minTemperature || temperature > maxTemperature) {
+function getTemperatureColorClass(temperature, location){
+	if (temperature < location.minTemperature || temperature > location.maxTemperature) {
 		return 'red';
 	} else{
 		return 'green';
 	}
 }
 
-function getHumidityColorClass (humidity){
+$('#intSettingsButton').on('click', function(){
+    $('#modal-main-int').css('display', "block");
+});
+
+$('#extSettingsButton').on('click', function(){
+    $('#modal-main-ext').css('display', "block");
+});
+
+
+$('.close').on('click', function(){
+    $('#modal-main-int').css('display', "none");
+    $('#modal-main-ext').css('display', "none");
+});
+
+window.onclick = function(event){
+	if (event.target == document.getElementById('modal-main-int') || 
+		event.target == document.getElementById('modal-main-ext')) {
+    	$('#modal-main-int').css('display', "none");
+        $('#modal-main-ext').css('display', "none");
+    }
+};
+
+function getHumidityColorClass (humidity, location){
 	if(!!humidity){
-		if (humidity < minHumidity || humidity > maxHumidity) {
+		if (humidity < location.minHumidity || humidity > location.maxHumidity) {
 			return 'red';
 		} else{
 			return 'green';
@@ -112,11 +144,117 @@ function getHumidityColorClass (humidity){
 	}
 }
 
+function setInputText(){
+	$('#intMinTemp').val(internalRange.minTemperature);
+	$('#intMaxTemp').val(internalRange.maxTemperature);
+
+	$('#intMinHumidity').val(internalRange.minHumidity);
+	$('#intMaxHumidity').val(internalRange.maxHumidity);
+
+	$('#extMinTemp').val(externalRange.minTemperature);
+	$('#extMaxTemp').val(externalRange.maxTemperature);
+
+	$('#extMinHumidity').val(externalRange.minHumidity);
+	$('#extMaxHumidity').val(externalRange.maxHumidity);
+};
+
 function initialize(){
 	hideLoader();
 	getData(refreshView);
+	setInputText();
+}
+
+ function validateValues(){
+ 	
+ 	var isValidated = false;
+
+	var intMinTemp = Number($('#intMinTemp').val()),
+		intMaxTemp = Number($('#intMaxTemp').val()),
+		extMinTemp = Number($('#extMaxTemp').val()),
+		extMaxTemp = Number($('#extMaxTemp').val());
+
+	if (intMinTemp > intMaxTemp || extMinTemp > extMaxTemp){
+		$('#errorMsg').text('A temperatura mínima é maior do que a temperatura máxima.');
+		return false;
+	}else{
+
+		var intMinHumidity = Number($('#intMinHumidity').val()),
+			intMaxHumidity = Number($('#intMaxHumidity').val()),
+			extMinHumidity = Number($('#extMinHumidity').val()),
+			extMaxHumidity = Number($('#extMaxHumidity').val());
+
+		if (intMinHumidity > intMaxHumidity || extMinHumidity > extMaxHumidity){
+			$('#errorMsg').text('A umidade mínima é maior do que a temperatura máxima.');
+			return false;
+		}else{
+			isValidated = true;
+		}
+
+		if (isValidated == true){
+			internalRange.minTemperature = $('#intMinTemp').val();
+			internalRange.maxTemperature = $('#intMaxTemp').val();
+			externalRange.minTemperature = $('#extMinTemp').val();
+			externalRange.maxTemperature = $('#extMaxTemp').val();
+			internalRange.minHumidity 	 = $('#intMinHumidity').val();
+			internalRange.maxHumidity 	 = $('#intMaxHumidity').val();
+			externalRange.minHumidity 	 = $('#extMinHumidity').val();
+			externalRange.maxHumidity 	 = $('#extMaxHumidity').val();
+			$('#errorMsg').text('');
+			return true;
+		}
+	}
 }
 
 $('.btnRefresh').on('click', function(){
 	getData(refreshView);
+});
+
+function getInput(event){
+	
+	if (event.which == 190 || event.which == 69){
+		event.preventDefault();
+		event.stopPropagation();
+		return false;
+	}
+
+	if (event.which	!= 188 && event.which != 8 && event.which != 46 && event.which != 9){
+		if (event.target.value.indexOf('.') == -1){
+			return event.target.value.length <= 2;
+		}else{
+			return event.target.value.split(',')[0].length < 2 && event.target.value.split(',')[1].length == 1;
+		}
+	}
+
+	if (event.target.validationMessage != ''){
+		$('#errorMsg').text(event.target.validationMessage);
+	}else{
+		for (var i = 0; i < event.target.parentNode.children.length - 1; i++) {
+			if (event.target.parentNode.children[i].validationMessage){
+				if (event.target.parentNode.children[i].validationMessage != ''){
+					$('#errorMsg').text(event.target.validationMessage);
+					return false;
+				}
+			}
+		}
+		$('#errorMsg').text('');
+		return true;
+	}
+}
+
+$('#setInternal').on('click', function(){
+	if (validateValues() && $('#errorMsg').text() == ''){
+		$('#modal-main-int').css('display', "none");
+		getData(refreshView);
+    	setInputText();
+    	console.log(internalRange);
+	}
+});
+
+$('#setExternal').on('click', function(){
+	if (validateValues() && $('#errorMsg').text() == ''){
+		$('#modal-main-ext').css('display', "none");
+		getData(refreshView);
+    	setInputText();
+    	console.log(externalRange);
+	}
 });
